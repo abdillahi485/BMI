14c6ec644be1bcf5736ee87987e39ac689b64d1e
Abdillahi485 <daakircadde143@gmail.com>
2023-10-29 15:01:05 +0300

    waan dhamaystiray

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculateBMI(weight, height) {
+    const bmi = weight / (height * height);
+  
+    return bmi
+  }
+
+const mohamedWeight = 75; 
+const mohamedHeight = 1.75; 
+const mohamedBMI = calculateBMI(mohamedWeight, mohamedHeight);
+
+const aliWeight = 68; 
+const aliHeight = 1.80; 
+const aliBMI = calculateBMI(aliWeight, aliHeight);
+
+if (mohamedBMI > aliBMI) {
+  console.log("Mohamed has a higher BMI");
+} else if (aliBMI > mohamedBMI) {
+  console.log("Ali has a higher BMI");
+} else {
+  console.log("Mohamed and Ali have the same BMI");
+}
